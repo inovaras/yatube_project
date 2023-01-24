a9ef68a4ca491d85e9cf64c0a7e78054d00347ee
Albina Gilmutdinova <inovaras@mail.ru>
2023-01-24 17:15:14 +0500

    enabled css

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,9 +2,12 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'posts'
 urlpatterns = [
     # Главная страница
     path('', views.index),
     # Страницы с отфильтрованными по группам постами
     path('group/<slug:slug>/', views.group_posts),
+    path('group/all_group_posts/', views.group_posts, name='all_group_posts'),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+# Импортируем загрузчик.
+from django.shortcuts import render
 
 # Create your views here.
 # Главная страница
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    title = 'Это главная страница проекта Yatube'
+    context = {
+        'title': title,
+        'text': 'Последние обновления на сайте'
+    }
+    return render(request, template, context)
 
 
 # Страница с постами
 def group_posts(request, slug):
-    return HttpResponse(f'Вы открыли страницy, '
-                        f'с отфильтрованными по группам {slug} постами.')
+    template = 'posts/group_list.html'
+    title = 'Здесь будет информация о группах проекта Yatube'
+    poem = ('Вчера Крокодил<br>улыбнулся так злобно,<br>Что мне до сих'
+            ' пор<br>за него неудобно.<br> Всем хорошего настроения :)<br>')
+    context = {
+        'title': title,
+        'text': 'Лев Толстой – зеркало русской революции.',
+        'poem': poem
+    }
+    return render(request, template, context)
